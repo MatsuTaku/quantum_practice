95a569bf9bd5c62b85bbfed5723fb8f651f66cec
MatsuTaku <tkm.matsumoto@gmail.com>
2019-03-27 18:22:02 +0900

    imprement nonnegative/binary matrix factorization.

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -1,40 +1,106 @@
 import numpy as np
+from numpy import linalg as la
 from blueqat import vqe, opt
-from blueqat.pauli import *
+from blueqat.pauli import qubo_bit as q
+import random
 
-def get_rating_error(r, p, q):
-    return r - np.dot(p.T, q)
+class MF_classic:
+    def __init__(self, R, K, steps=5000, alpha=0.0002, beta=0.02, threshold=0.001):
+        self.R = R
+        self.K = K
+        self.steps = steps
+        self.alpha = alpha
+        self.beta = beta
+        self.threshold = threshold
 
-def get_error(r, p, q, beta):
-    error = 0.0
-    for i in range(len(r)):
-        for j in range(len(r[0])):
-            error += get_rating_error(r[i][j], p[:, i], q[:, j]) ** 2
-            error += beta * (np.linalg.norm(p[:, i])**2 + np.linalg.norm(q[:, j])**2)
-    return error
+    def get_rating_error(self, r, p, q):
+        return r - np.dot(p.T, q)
 
-def matrix_factorization_classic(R, K, steps=5000, alpha=0.0002, beta=0.02, threshold=0.001):
-    P = np.random.rand(K, len(R))
-    Q = np.random.rand(K, len(R[0]))
-    for step in range(steps):
-        for i in range(len(R)):
-            for j in range(len(R[0])):
-                if R[i][j] == 0:
-                    continue
-                error = get_rating_error(R[i][j], P[:, i], Q[:, j])
-                for k in range(K):
-                    delta_p = alpha * (2 * error * Q[k, j])
-                    delta_q = alpha * (2 * error * P[k, i])
-                    P[k, i] += delta_p
-                    Q[k, j] += delta_q
+    def get_error(self, r, p, q, beta):
+        error = 0.0
+        for i in range(len(r)):
+            for j in range(len(r[0])):
+                error += self.get_rating_error(r[i][j], p[:, i], q[:, j]) ** 2
+                error += beta * (np.linalg.norm(p[:, i])**2 + np.linalg.norm(q[:, j])**2)
+        return error
 
-        if get_error(R, P, Q, beta) < threshold:
-            break
-    return P, Q
+    def run(self):
+        n, m = len(self.R), len(self.R[0])
+        P = np.random.rand(self.K, n)
+        Q = np.random.rand(self.K, m)
+        for step in range(self.steps):
+            for i in range(n):
+                for j in range(m):
+                    if self.R[i][j] == 0:
+                        continue
+                    error = self.get_rating_error(self.R[i][j], P[:, i], Q[:, j])
+                    for k in range(self.K):
+                        delta_p = self.alpha * (2 * error * Q[k, j])
+                        delta_q = self.alpha * (2 * error * P[k, i])
+                        P[k, i] += delta_p
+                        Q[k, j] += delta_q
 
-def matrix_factorization(R, K, steps=4, alpha=0.0002, beta=0.02):
-    W = np.random.rand(K, len(R))
-    H = np.random.rand(K, len(R[0])) % 2
+            error = self.get_error(self.R, P, Q, self.beta)
+            if error < self.threshold ** 2:
+                break
+        return P, Q
+
+
+class MF_quantam:
+    def __init__(self, V, k, steps=50, alpha=0.02, beta=0.02, threshold=0.001):
+        self.V = V
+        self.k = min(k, 1024)
+        self.steps = steps
+        self.alpha = alpha
+        self.beta = beta
+        self.threshold = threshold
+
+    def run(self):
+        n, m = len(self.V), len(self.V[0])
+        W = np.random.rand(n, self.k)   # W is nonnegative matrix
+        H = np.random.rand(self.k, m)       # H is binary matrix
+        for step in range(self.steps):
+            # Solve W
+            for i in range(n):
+                for j in range(m):
+                    if self.V[i][j] == 0:
+                        continue
+                    error = (self.V[i][j] - np.dot(W[i], H[:, j])) ** 2
+                    error += self.beta * ((la.norm(W[i]) ** 2))
+                    for kk in range(self.k):
+                        W[i][kk] += self.alpha * 2 * error * H[kk][j]
+                        if W[i][kk] < 0:
+                            W[i][kk] = 0
+
+            # Solve H
+            a = np.zeros((m, self.k))
+            for i in range(m):
+                for l in range(n):
+                    vli = self.V[l][i]
+                    if vli == 0:
+                        continue
+                    for j in range(self.k):
+                        wlj = W[l][j]
+                        a[i][j] += wlj * (wlj - vli*2)
+            b = np.zeros((self.k, self.k))
+            for i in range(self.k):
+                for j in range(i+1, self.k):
+                    b[i][j] = np.dot(W[:, i], W[:, j])
+            for n in range(m):
+                hamiltonian = 0
+                for i in range(self.k):
+                    hamiltonian += q(i) * a[n][i]
+                    for j in range(i+1, self.k):
+                        hamiltonian += q(i)*q(j)*b[i][j]
+
+                result = vqe.Vqe(vqe.QaoaAnsatz(hamiltonian, 2)).run()
+                H[:, j] = result.most_common(1)[0][0]
+
+            error = la.norm(self.V - np.dot(W, H))
+            if error < self.threshold:
+                break
+
+        return W, H
 
 
 if __name__ == '__main__':
@@ -46,11 +112,15 @@ if __name__ == '__main__':
         [0,1,5,4]
     ])
     # %%
-    nP, nQ = matrix_factorization_classic(R, 2)
+    nP, nQ = MF_classic(R, int(len(R[0])*0.5)).run()
+    print(nP.T)
+    print(nQ)
     nR = np.dot(nP.T, nQ)
     print(nR)
 
-    # %% quantm
-    nP, nQ = matrix_factorization(R, 2)
-    nR = np.dot(nP.T, nQ)
+    # %% qaoa
+    nP, nQ = MF_quantam(R, int(len(R[0])*0.6)).run()
+    print(nP)
+    print(nQ)
+    nR = np.dot(nP, nQ)
     print(nR)
